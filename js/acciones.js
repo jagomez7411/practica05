451987c1d923362fa89321769542ea7d1406aab2
rt <rt>
2013-11-12 16:58:24 -0600

    subiendo1

---
+++ js/acciones.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		$('#.datos Table td').eq(1).Text(device.name);
+		
+			
+			
+	},false);//ready device
+});//document
