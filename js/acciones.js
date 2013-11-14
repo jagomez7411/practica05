1ed0b038bd0dbacf0aa4db4ba6fcd535cb5f70e7
antonio gomez <antonio gomez>
2013-11-14 15:54:11 -0600

    subiendo6

--- js/acciones.js
+++ js/acciones.js
@@ -3,9 +3,9 @@
 $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
 		$('#disp table td').eq(1).text(device.name);
-		$('#disp table td').eq(3).text(device.plataform);
-		$('#disp table td').eq(5).text(device.version);
-		$('#disp table td').eq(7).text(device.cordova);
+		$('#disp table td').eq(3).text(device.phonegap);
+		$('#disp table td').eq(5).text(device.platform);
+		$('#disp table td').eq(7).text(device.version);
 		$('#disp table td').eq(9).text(device.uuid);
 			
 			
