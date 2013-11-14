cadf24cd74b2db54e04f377f4efb84b4caf7b377
antonio gomez <antonio gomez>
2013-11-14 15:32:07 -0600

    subiendo4

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
-		$('#datos Table td').eq(1).text(device.name);
+		$('#disp table td').eq(1).text(device.name);
 		
 			
 			
