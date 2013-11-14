d790b9bbcc4f321534e6b4b75f4367a210e58188
antonio gomez <antonio gomez>
2013-11-14 15:50:14 -0600

    subiendo5

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,10 @@
 $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
 		$('#disp table td').eq(1).text(device.name);
-		
+		$('#disp table td').eq(3).text(device.plataform);
+		$('#disp table td').eq(5).text(device.version);
+		$('#disp table td').eq(7).text(device.cordova);
+		$('#disp table td').eq(9).text(device.uuid);
 			
 			
 	},false);//ready device
