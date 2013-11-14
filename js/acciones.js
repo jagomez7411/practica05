d06503ebb8e344ec2475d922f5507f109f1135fd
antonio gomez <antonio gomez>
2013-11-14 16:07:02 -0600

    subiendo8

--- js/acciones.js
+++ js/acciones.js
@@ -2,11 +2,11 @@
 
 $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
-		$('#disp table td').eq(1).text(device.name);
-		$('#disp table td').eq(3).text(device.cordova);
-		$('#disp table td').eq(5).text(device.platform);
-		$('#disp table td').eq(7).text(device.version);
-		$('#disp table td').eq(9).text(device.uuid);
+		$('#disp table td').eq(3).text(device.name);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
 			
 			
 	},false);//ready device
